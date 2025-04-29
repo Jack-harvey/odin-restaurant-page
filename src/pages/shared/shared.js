cbe3da4d36972375bd2b36fecaee89b7fe443965
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-04-29 18:05:27 +1000

    Removed instance of innerHTML

--- src/pages/shared/shared.js
+++ src/pages/shared/shared.js
@@ -1,15 +1,7 @@
 const content = document.querySelector("#content");
 
 export function clearContent() {
-  content.innerHTML = "";
-}
-
-export function navEventListener() {
-  window.addEventListener("load", () => {
-    const navBar = document.querySelector("nav");
-
-    navBar.addEventListener("click", (event) => {
-      console.log(event.target.id);
-    });
-  });
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
 }
